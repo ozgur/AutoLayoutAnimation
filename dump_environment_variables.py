6b48f5bcb15d81cec5c7dfab424dd35a89b035a5
ozgur <ozgur_vatansever@homedepot.com>
2015-11-06 19:52:08 -0800

    updated build script

--- dump_environment_variables.py
+++ dump_environment_variables.py
@@ -5,14 +5,12 @@ import os
 import ConfigParser
 
 if __name__ == "__main__":
-    if not "XCS_DERIVED_DATA_DIR" in os.environ:
-        return
-
-    with open(os.path.join(os.environ.get("XCS_DERIVED_DATA_DIR"), "env"), "w") as f:
-        config = ConfigParser.SafeConfigParser()
-        config.optionxform = str
-        config.add_section("Xcode")
-        config.set("Xcode", "ProductName", os.environ.get("FULL_PRODUCT_NAME", ""))
-        config.set("Xcode", "Configuration", os.environ.get("CONFIGURATION", ""))
-        config.write(f)
-        print "File is: %s" % os.path.join(os.environ.get("XCS_DERIVED_DATA_DIR"), "env")
+    if "XCS_DERIVED_DATA_DIR" in os.environ:
+        with open(os.path.join(os.environ["XCS_DERIVED_DATA_DIR"], "env"), "w") as f:
+            config = ConfigParser.SafeConfigParser()
+            config.optionxform = str
+            config.add_section("Xcode")
+            config.set("Xcode", "ProductName", os.environ.get("FULL_PRODUCT_NAME", ""))
+            config.set("Xcode", "Configuration", os.environ.get("CONFIGURATION", ""))
+            config.write(f)
+            print "File is: %s" % os.path.join(os.environ.get("XCS_DERIVED_DATA_DIR"), "env")
