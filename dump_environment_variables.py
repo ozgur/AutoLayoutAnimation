04fdbd7b96932ce29e19f3588ae7c0be1603ddbe
ozgur <ozgur_vatansever@homedepot.com>
2015-11-06 20:46:55 -0800

    updated build script

--- dump_environment_variables.py
+++ dump_environment_variables.py
@@ -4,13 +4,31 @@
 import os
 import ConfigParser
 
+
+def main():
+    def get_environment_variable_if_possible(key, default=None):
+        return os.environ.get(key, default)
+
+    if not get_environment_variable_if_possible("XCS_DERIVED_DATA_DIR"):
+        return
+
+    project_root = get_environment_variable_if_possible("XCS_DERIVED_DATA_DIR")
+    with open(os.path.join(project_root, "env"), "w") as f:
+        config = ConfigParser.SafeConfigParser()
+        config.optionxform = str
+        config.add_section("Xcode")
+
+        app_name = get_environment_variable_if_possible("FULL_PRODUCT_NAME", "")
+        config.set("Xcode", "ProductName", app_name)
+
+        cfg = get_environment_variable_if_possible("CONFIGURATION", "")
+        config.set("Xcode", "Configuration", cfg)
+
+        target = get_environment_variable_if_possible("IPHONEOS_DEPLOYMENT_TARGET", "")
+        config.set("Xcode", "DeploymentTarget", target)
+
+        config.write(f)
+
+
 if __name__ == "__main__":
-    if "XCS_DERIVED_DATA_DIR" in os.environ:
-        with open(os.path.join(os.environ["XCS_DERIVED_DATA_DIR"], "env"), "w") as f:
-            config = ConfigParser.SafeConfigParser()
-            config.optionxform = str
-            config.add_section("Xcode")
-            config.set("Xcode", "ProductName", os.environ.get("FULL_PRODUCT_NAME", ""))
-            config.set("Xcode", "Configuration", os.environ.get("CONFIGURATION", ""))
-            config.write(f)
-            print "File is: %s" % os.path.join(os.environ.get("XCS_DERIVED_DATA_DIR"), "env")
+    main()
