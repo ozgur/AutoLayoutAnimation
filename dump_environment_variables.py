295bd4bb82abff6d5b4197a5bf55af7723562640
ozgur <ozgur_vatansever@homedepot.com>
2015-11-06 19:17:59 -0800

    updated build script

--- dump_environment_variables.py
+++ dump_environment_variables.py
@@ -5,12 +5,13 @@ import os
 import tempfile
 import ConfigParser
 
-if __name__ == '__main__':
-    path = os.path.join(os.environ.get('TMPDIR', tempfile.gettempdir()), 'env')
-    with open(path, 'w') as f:
+if __name__ == "__main__":
+    path = os.path.join(os.environ.get("TMPDIR", tempfile.gettempdir()), "env")
+    with open(path, "w") as f:
         config = ConfigParser.SafeConfigParser()
         config.optionxform = str
-        config.add_section('Xcode')
-        config.set('Xcode', 'ProductName', os.environ.get('FULL_PRODUCT_NAME', ''))
-        config.set('Xcode', 'Configuration', os.environ.get('CONFIGURATION', ''))
+        config.add_section("Xcode")
+        config.set("Xcode", "ProductName", os.environ.get("FULL_PRODUCT_NAME", ""))
+        config.set("Xcode", "Configuration", os.environ.get("CONFIGURATION", ""))
         config.write(f)
+        print "File is: %s" % path
