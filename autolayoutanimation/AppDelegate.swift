c55947c56b594e2da378dacbb311795f31f2bb4b
ozgur <ozgur_vatansever@homedepot.com>
2015-11-10 15:34:17 -0800

    some changes

--- autolayoutanimation/AppDelegate.swift
+++ autolayoutanimation/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
-
+    
     return true
   }
 
